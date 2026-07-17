ef32229db1602c4f2e5f46cfa3c347036ff73b8d
Chetan Mehra <chetanmehra233@gmail.com>
2026-07-17 20:47:15 +0530

    Api gateway implementation

---
+++ api-gateway/src/main/java/com/apigateway/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.apigateway.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route("product-service", r -> r
+						.path("/api/v1/product/**")
+						.uri("lb://PRODUCT-SERVICE"))
+				.build();
+	}
+}
